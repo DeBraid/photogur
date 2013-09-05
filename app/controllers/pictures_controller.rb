9d651b9f36c11ff5e8664d4b541ff17865229bed
DeBraid <de.braid@gmail.com>
2013-09-05 16:13:34 -0400

    pics now display with title & author

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,22 @@
 class PicturesController < ApplicationController
 
+    def index
+        @pictures = [
+        { 
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"},
+        { 
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"},
+        {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"}
+                  ]
+    end
+
     def show 
       @pictures = [
         { 
@@ -24,22 +41,30 @@ class PicturesController < ApplicationController
         @pictures.create(params[:title][:artist][:url])
     end
 
-  #rails4 code
-    # def create
-    #     @picture = Picture.new(params.require(:picture).permit(:title, :artist, :url))
-    #     if @picture.save
-    #         redirect_to pictures_url
-    #     else
-    #         render :new
-    #     end
-    # end
-
-#rails3 from tutorial
+  
     def create
-        render :text => "Saving a Picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+        @picture = Picture.new(params.require(:picture).permit(:title, :artist, :url))
+        if @picture.save
+            redirect_to pictures_url
+        else
+            render :new
+        end
     end
 
+# #rails3 from tutorial
+#     def create
+#         @picture = Picture.new(picture_params)
+#         render :text => "Saving a Picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+#     end
+
     def update
+        @picture = Picu
+    end
+    
+    def edit
+    end
+
+    def destroy
     end
 
 end
