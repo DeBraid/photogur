817ceb8c73e4a1c04689e478326ce593f6cd6927
DeBraid <de.braid@gmail.com>
2013-09-05 20:36:26 -0400

    database added and seeded

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,39 +1,41 @@
 class PicturesController < ApplicationController
 
     def index
-        @pictures = [
-        { 
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"},
-        { 
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"},
-        {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"}
-                  ]
+        @pictures = Pictures.all 
+        #the above takes place of all below now that its in DB
+        # { 
+        # :title  => "The old church on the coast of White sea",
+        # :artist => "Sergey Ershov",
+        # :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"},
+        # { 
+        # :title  => "Sea Power",
+        # :artist => "Stephen Scullion",
+        # :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"},
+        # {
+        # :title  => "Into the Poppies",
+        # :artist => "John Wilhelm",
+        # :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"}
+        #           ]
     end
 
     def show 
-      @pictures = [
-        { 
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"},
-        { 
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"},
-        {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"}
-                  ]
+      @pictures = Pictures.all(params[:id])
+      # [
+      #   { 
+      #   :title  => "The old church on the coast of White sea",
+      #   :artist => "Sergey Ershov",
+      #   :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"},
+      #   { 
+      #   :title  => "Sea Power",
+      #   :artist => "Stephen Scullion",
+      #   :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"},
+      #   {
+      #   :title  => "Into the Poppies",
+      #   :artist => "John Wilhelm",
+      #   :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"}
+      #             ]
 
-      @picture = @pictures[params[:id].to_i]
+      # @picture = @pictures[params[:id].to_i]
     end
 
 
@@ -58,13 +60,20 @@ class PicturesController < ApplicationController
 #     end
 
     def update
-        @picture = Picu
+        @picture = Picture.find(params[:id])
+        if @picture.update_attributes(params.require(:picture).permit(:title, :artist, :url))
+            redirect_to "/pictures/#{@picture.id}"
+        else
+            render :edit
+        end
     end
     
     def edit
+        @picture = Picture.find(params[:id])
     end
 
     def destroy
+        @picture = Picture.destroy(params[:id])
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130905234352) do
+
+  create_table "pictures", :force => true do |t|
+    t.string   "artist"
+    t.string   "title"
+    t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
