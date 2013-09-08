33971dd8276fdcbaf48211efda2f780afb6acc7b
DeBraid <de.braid@gmail.com>
2013-09-08 16:59:15 -0400

    fixed index.html to run each loop properly

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,21 +1,22 @@
 class PicturesController < ApplicationController
 
     def index
-        @pictures = Pictures.all 
+        # @pictures = Pictures.all 
         #the above takes place of all below now that its in DB
-        # { 
-        # :title  => "The old church on the coast of White sea",
-        # :artist => "Sergey Ershov",
-        # :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"},
-        # { 
-        # :title  => "Sea Power",
-        # :artist => "Stephen Scullion",
-        # :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"},
-        # {
-        # :title  => "Into the Poppies",
-        # :artist => "John Wilhelm",
-        # :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"}
-        #           ]
+        @pictures = [
+          { 
+          :title  => "The old church on the coast of White sea",
+          :artist => "Sergey Ershov",
+          :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"},
+          { 
+          :title  => "Sea Power",
+          :artist => "Stephen Scullion",
+          :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"},
+          {
+          :title  => "Into the Poppies",
+          :artist => "John Wilhelm",
+          :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"}
+                  ]
     end
 
     def show 
